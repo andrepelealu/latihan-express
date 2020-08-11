2d96cf90009e8bb16bf44ef7cdd870257abe6d48
andre <projects.andrepelealu@gmail.com>
2020-08-11 16:37:23 +0700

    test

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ require('dotenv').config()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/users', usersRouter);
 
-app.listen(process.env.PORT || 4000)
+app.listen(process.env.PORT || 3000)
 
 module.exports = app;
